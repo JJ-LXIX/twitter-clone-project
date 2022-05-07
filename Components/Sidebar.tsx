4eca3ee9812ff59921fee728fd44bd6bd946dabe
Jagrath <jagrathshettylxix@gmail.com>
2022-05-07 10:14:42 +0530

    sidebar (no functionality) added

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -8,13 +8,25 @@ import {
     MailIcon,
     UserIcon,
     HomeIcon,} from '@heroicons/react/outline';
+import SidebarRow from './SidebarRow';
+import Image from 'next/image';
 
 const Sidebar = () => {
   return (
-    <div>
-        <img className=' w-10 h-10' src="https://links.papareact.com/drq" alt="twitter logo" />
+    <div className="flex flex-col ">
+        <Image  width={40}  height={40} layout='fixed' src="https://links.papareact.com/drq" alt="twitter logo" />
+        <SidebarRow  Icon = {HomeIcon} title='Home'/>
+        <SidebarRow Icon = {HashtagIcon} title='Explore'/>
+        <SidebarRow Icon = {BellIcon} title='Notifications'/>
+        <SidebarRow Icon = {MailIcon} title='Messages'/>
+        <SidebarRow Icon = {BookmarkIcon} title='Bookmarks'/>
+        <SidebarRow Icon = {CollectionIcon} title='Lists'/>
+        <SidebarRow Icon = {UserIcon} title='Sign In'/>
+
+        <SidebarRow Icon = {DotsCircleHorizontalIcon} title='More'/>
     </div>
   )
+    
 }
 
 export default Sidebar
