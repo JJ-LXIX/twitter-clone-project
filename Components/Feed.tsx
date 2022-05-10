9125cc26367d8050cdb37f8876bbd3ee25281e4c
Jagrath <jagrathshettylxix@gmail.com>
2022-05-10 12:34:17 +0530

    refresh onClick added

--- Components/Feed.tsx
+++ Components/Feed.tsx
@@ -1,19 +1,27 @@
 import { RefreshIcon } from '@heroicons/react/outline'
-import React from 'react'
+import React, { useState } from 'react'
 import { Tweet } from '../typing'
 import TweetBox from './TweetBox'
 import TweetComponent from '../Components/Tweet';
+import { fetchTweets } from '../utils/fetchTweets';
 interface Props {
   tweets : Tweet[]
 }
 
-const Feed = ({tweets}:Props) => {
+const Feed = ({tweets :tweetsProp}:Props) => {
+  const [ tweets, setTweets] = useState<Tweet[]>(tweetsProp)
   console.log(tweets);
+
+  const handleRefresh = async () => {
+    const tweets = await fetchTweets();
+    setTweets(tweets);
+  }
+
   return (
     <div className='col-span-7 border-x lg:col-span-5'>
         <div className='flex items-center justify-between'>
             <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-            <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter__blue  transition-all duration-500 ease-out 
+            <RefreshIcon onClick={handleRefresh} className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter__blue  transition-all duration-500 ease-out 
             hover:-rotate-180 active:scale-75"/>
         </div>
 
