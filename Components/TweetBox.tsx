b4be03901382cff8fe88db88e123d1840ddf4a52
Jagrath <jagrathshettylxix@gmail.com>
2022-05-10 18:01:43 +0530

    adding ImageURL to tweet functionality added

--- Components/TweetBox.tsx
+++ Components/TweetBox.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {
   CalendarIcon,
   EmojiHappyIcon,
@@ -12,7 +12,20 @@ import { useSession } from 'next-auth/react'
 const TweetBox = () => {
 
   const [input,setInput] = useState<string>('')
+  const [image, setImage] = useState<string>('')
+  const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false)
+
+  const imageInputRef = useRef<HTMLInputElement>(null)
+
   const {data:session} = useSession()
+
+  const addImageToTweet = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    if(!imageInputRef.current?.value) return;
+    setImage(imageInputRef.current.value)
+    imageInputRef.current.value = '';
+    setImageBoxOpen(false);
+  }
 
   return (
     <div className='flex space-x-2 p-5'>
@@ -24,7 +37,7 @@ const TweetBox = () => {
 
             <div className='flex items-center'>
               <div className='flex space-x-2 flex-1 text-twitter__blue'>  
-                <PhotographIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
+                <PhotographIcon onClick={()=>setImageBoxOpen(!imageBoxOpen)} className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-125 active:scale-90'/>
                 <SearchCircleIcon className='h-5 w-5'/>
                 <EmojiHappyIcon className='h-5 w-5'/>
                 <CalendarIcon className='h-5 w-5'/>
@@ -33,6 +46,17 @@ const TweetBox = () => {
 
               <button disabled={!input || !session } className='rounded-full bg-twitter__blue text-white px-5 py-2 font-bold disabled:opacity-40'>Tweet</button>
             </div>
+
+          {
+            imageBoxOpen && (
+              <form  className="rounded-lg mt-5 flex bg-twitter__blue py-2 px-4">
+                <input ref={imageInputRef} className='flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white ' type="text" placeholder="Enter Image URL ..."/>
+                <button type='submit' onClick={addImageToTweet} className='font-bold text-white'>Add Image</button>
+              </form>
+            )
+          }
+
+          {image && <img className='mt-10  h-40 bg-slate-200/20 w-full  rounded-xl object-contain shadow-lg' src={image} alt="added image"/>}
 
           </form>
         </div>
